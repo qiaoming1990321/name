b3cb81201045712e34545a6d596ec7862769317c
张齐 <Cleven@Cleven-2.local>
2016-11-24 16:15:09 +0800

    又删了两行注释

--- git_test/git_test/AppDelegate.m
+++ git_test/git_test/AppDelegate.m
@@ -16,7 +16,6 @@
 
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-    // Override point for customization after application launch.
     return YES;
 }
 
@@ -31,7 +30,7 @@
 
 
 - (void)applicationWillEnterForeground:(UIApplication *)application {
-    // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
+
 }
 
 
